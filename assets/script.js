c66d0e5950386ce75c4a67907a5abd742329fa26
Nick Sprague <n.sprague18@gmail.com>
2024-02-13 22:08:07 -0500

    fixed console errors on save button and date/time display

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,23 @@
+console.log('testing')
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// DONE
+// in the html. ||DONE||
 
-//USED https://learn.jquery.com/using-jquery-core/document-ready/ TO LEARN ABOUT HOW $(document).ready() DETECTS THE READINESS STATE OF A WEBPAGE
+// USED https://learn.jquery.com/using-jquery-core/document-ready/ TO LEARN ABOUT HOW $(document).ready() DETECTS THE READINESS STATE OF A WEBPAGE
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // WORKING
+  // useful when saving the description in local storage? ||DONE||
 
   // USED https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this TO LEARN MORE ABOUT (this)
-  $('.saveBtn').onclick(function() {
+
+  // WAS GETTING ERRORS IN THE CONSOLE WAS TRYING TO USE FUNCTION onclick(function()) INSTEAD OF .on('click', function())
+  // ERROR RESOLVED || SECTION COMPLETE
+  $('.saveBtn').on('click', function() {
     console.log(this)
     var sibling = $(this).siblings('.siblingValue').value()
     var parent = $(this).parent().attr('id')
@@ -30,11 +33,13 @@ $(document).ready(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-  // TODO: Add code to display the current date in the header of the page.
-  // WORKING
-
-  var currentDate = (moment().format('MMMM D, YYYY'))
-  $('#currentDay').text(currentDate);
-  console.log(currentDate)
+  // TODO: Add code to display the current date in the header of the page. ||DONE||
+  
+  //USED https://day.js.org/docs/en/display/format TO FIGURE OUT DISPLAY AND FORMATTING ISSUES
+  // FINALLY FIXED, WAS USING WRONG FORMAT AND FUNCTION
+  // ERROR RESOLVED || SECTION COMPLETE
+  var currentDay = dayjs().format('DD MMM. YYYY H:mm:ss')
+  $('#currentDay').text(currentDay)
+  // console.log(currentDay)
 
 });
