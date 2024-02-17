33dd1e29a3bdface558b900ea639748bb250f88e
Nick Sprague <n.sprague18@gmail.com>
2024-02-17 12:57:01 -0500

    added extra html to take schedule until 11pm, still working on getting JS to interact with html the right way to show past present future

--- assets/script.js
+++ assets/script.js
@@ -19,8 +19,9 @@ $(document).ready(function () {
   // ERROR RESOLVED || SECTION COMPLETE
   $('.saveBtn').on('click', function() {
     console.log(this)
-    var sibling = $(this).siblings('.siblingValue').value()
-    var parent = $(this).parent().attr('id')
+    let sibling = $(this).siblings('.siblingValue').value()
+    let parent = $(this).parent().attr('id')
+    localStorage.setItem(parent, sibling)
   })
   
   // TODO: Add code to apply the past, present, or future class to each time
@@ -32,9 +33,11 @@ $(document).ready(function () {
   // USED https://day.js.org/docs/en/get-set/hour AND https://day.js.org/docs/en/display/difference TO WRITE FUNCTION TO FIND DIFFERENCE BETWEEN CURRENT HOUR AND DISPLAYED HOUR
   // NOT GETTING AN ERROR, BUT ALSO NOT WORKING YET
   function workingHours() {
-    var getHour = dayjs().hour()
+    // GETTING THE CURRENT HOUR
+    let getHour = dayjs().hour()
+    // ITERATING OVER ALL ELEMENTS WITH THE CLASS OF .TIME-BLOCK
     $('.time-block').each(function() {
-      var timeBlockHour = parseInt($(this).attr('id').split('hour')[1])
+      let timeBlockHour = parseInt($(this).attr('id').split('hour')[1]);
       console.log(timeBlockHour, getHour)
 
       if (timeBlockHour < getHour) {
@@ -55,27 +58,29 @@ $(document).ready(function () {
     })
   }
 
+  workingHours()
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this? ||WORKING||
 
   // DOESNT CURRENTLY WORK, TROUBLESHOOTING
-  $('#hour-9, siblingValue').hour().val(localStorage.getItem('#hour-9'))
-  $('#hour-10, siblingValue').hour().val(localStorage.getItem('#hour-10'))
-  $('#hour-11, siblingValue').hour().val(localStorage.getItem('#hour-11'))
-  $('#hour-12, siblingValue').hour().val(localStorage.getItem('#hour-12'))
-  $('#hour-1, siblingValue').hour().val(localStorage.getItem('#hour-1'))
-  $('#hour-2, siblingValue').hour().val(localStorage.getItem('#hour-2'))
-  $('#hour-3, siblingValue').hour().val(localStorage.getItem('#hour-3'))
-  $('#hour-4, siblingValue').hour().val(localStorage.getItem('#hour-4'))
-  $('#hour-5, siblingValue').hour().val(localStorage.getItem('#hour-5'))
+  $('#0900, .siblingValue').val(localStorage.getItem('#0900'))
+  $('#1000, .siblingValue').val(localStorage.getItem('#1000'))
+  $('#1100, .siblingValue').val(localStorage.getItem('#1100'))
+  $('#1200, .siblingValue').val(localStorage.getItem('#1200'))
+  $('#1300, .siblingValue').val(localStorage.getItem('#1300'))
+  $('#1400, .siblingValue').val(localStorage.getItem('#1400'))
+  $('#1500, .siblingValue').val(localStorage.getItem('#1500'))
+  $('#1600, .siblingValue').val(localStorage.getItem('#1600'))
+  $('#1700, .siblingValue').val(localStorage.getItem('#1700'))
   
   // TODO: Add code to display the current date in the header of the page. ||DONE||
   
   //USED https://day.js.org/docs/en/display/format TO FIGURE OUT DISPLAY AND FORMATTING ISSUES
   // FINALLY FIXED, WAS USING WRONG FORMAT AND FUNCTION
   // ERROR RESOLVED || SECTION COMPLETE
-  var currentDay = dayjs().format('DD MMM. YYYY H:mm:ss')
+  const currentDay = dayjs().format('DD MMM. YYYY H:mm:ss')
   $('#currentDay').text(currentDay)
   // console.log(currentDay)
 
